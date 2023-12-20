70591b78d00d76b0f93c468b67c21bceabbc25d9
TDD788 <103203291+TDD788@users.noreply.github.com>
2023-12-19 21:04:25 -0300

    Neopackage.sh

--- neopackage.sh
+++ neopackage.sh
@@ -1,9 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Download neofetch
-curl -LO https://github.com/dylanaraps/neofetch/raw/master/neofetch
-chmod +x neofetch
-mv neofetch /data/data/com.termux/files/usr/bin/
+pkg i neofetch
 
 # Download neologo
 curl -LO https://github.com/TDD788/Plugins/raw/main/neologo
